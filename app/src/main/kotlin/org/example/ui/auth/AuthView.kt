562d0012714e971471feeede67e43fe2c8abe754
Sasha_Makaka <demina2000.alexa@gmail.com>
2026-04-23 18:04:11 +0300

    испр

--- app/src/main/kotlin/org/example/ui/auth/AuthView.kt
+++ app/src/main/kotlin/org/example/ui/auth/AuthView.kt
@@ -39,7 +39,7 @@ fun AuthView(
                 style = MaterialTheme.typography.titleLarge
             )
 
-            Divider(color = ColorDivider)
+            HorizontalDivider(color = ColorDivider)
 
             OutlinedTextField(
                 value = login,
