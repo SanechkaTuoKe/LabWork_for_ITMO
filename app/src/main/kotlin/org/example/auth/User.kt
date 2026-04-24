4526ccdc65de54ab66d46e1352532652d89c4feb
Sasha_Makaka <demina2000.alexa@gmail.com>
2026-04-25 00:01:15 +0300

    чтобы был исходник

--- app/src/main/kotlin/org/example/auth/User.kt
+++ app/src/main/kotlin/org/example/auth/User.kt
@@ -22,3 +22,11 @@ data class User(
         return passwordHash == hashPassword(password)
     }
 }
+//import java.security.MessageDigest
+//import java.util.Base64
+//fun main() {
+//        val md = MessageDigest.getInstance("SHA-256")
+//        val input = "test".toByteArray(Charsets.UTF_8)
+//        val bytes = md.digest(input)
+//        println(Base64.getUrlEncoder().encodeToString(bytes))
+//} - брала отсюда https://stackoverflow.com/questions/61465494/sha256-different-hash-in-nodejs-and-kotlin
